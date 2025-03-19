fe42650f33c001f62488dec31c47240cc9cc09b3
Branislav Bogosavac <97567320+branislav999@users.noreply.github.com>
2025-03-18 23:51:41 -0600

    'donew4'

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
+import { DocumentItemComponent } from '../document-item/document-item.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'cms-document-list',
-  imports: [],
+  imports: [DocumentItemComponent, CommonModule],
   templateUrl: './document-list.component.html',
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+
+
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document('1', 'Projektni Plan', 'Detaljan plan projekta', 'https://idemobrejako.com/projektni-plan', []),
+    new Document('2', 'Zapisnik sa Sastanka', 'Beleške sa timskog sastanka', 'https://staimaburaz.com/zapisnik-sastanka', []),
+    new Document('3', 'Izveštaj o Budžetu', 'Kvartalni izveštaj o budžetu', 'https://tusamjabe.com/budzet-izvestaj', []),
+    new Document('4', 'Dizajn Makete', 'UI/UX dizajn makete', 'https://radimgradim.com/dizajn-makete', [])
+  ];
+
+  onSelectedDocument(document: Document) {
+
+    this.selectedDocumentEvent.emit(document);
+  }
 
 }
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DocumentDetailComponent } from './document-detail/document-detail.component';
 import { DocumentListComponent } from './document-list/document-list.component';
+import { Document } from './document.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'cms-documents',
-  imports: [DocumentDetailComponent, DocumentListComponent],
+  imports: [DocumentDetailComponent, DocumentListComponent, CommonModule],
   templateUrl: './documents.component.html',
   styleUrl: './documents.component.css'
 })
 export class DocumentsComponent {
+
+  selectedDocument!: Document;
 
 }
